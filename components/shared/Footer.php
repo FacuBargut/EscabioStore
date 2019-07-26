2a2b2fda53cfbee5f1db7a4c4b5bcfa050a7fa58
Facundo Bargut <facubargut@gmail.com>
2019-07-26 02:44:56 -0300

    Primer commit

---
+++ components/shared/Footer.php
@@ -0,0 +1,13 @@
+<div class="col-md-12 bg-grey footer-wrapper">
+        Footer
+</div>
+
+
+<style scoped>
+.footer-wrapper{
+    background:black;
+    text-align: center;
+    color: white;
+    
+}
+</style>
---
+++ components/shared/Header.php
@@ -0,0 +1,37 @@
+<!-- Header     -->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="navbarLeft col-md-3">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <a class="navbar-brand" href="#">LOGO</a>
+                <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
+            </div>
+            
+
+            <form class="form-inline col-md-5">
+                    <input class="form-control" id="Search" type="search" placeholder="Busar...">
+            </form>
+
+            <div class="collapse navbar-collapse col-md-4" id="navbarTogglerDemo03">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Entra o registrate</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Mi carrito</a>
+                    </li>
+                </ul>
+
+            </div>
+        </nav>
+
+<style scope>
+#Search{
+    width: 100%;
+}
+
+.collapse ul{
+    margin-left: 40%;
+}
+</style>
