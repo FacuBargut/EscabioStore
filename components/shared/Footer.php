39751487d249fa8b491e6b5bb47c654b91cd21d4
Facundo Bargut <facubargut@gmail.com>
2019-07-30 16:43:29 -0300

    Commit a la rama facu

--- components/shared/Footer.php
+++ components/shared/Footer.php
@@ -1,13 +1,4 @@
 <div class="col-md-12 bg-grey footer-wrapper">
-        Footer
+        <p>Footer</p>
 </div>
 
-
-<style scoped>
-.footer-wrapper{
-    background:black;
-    text-align: center;
-    color: white;
-    
-}
-</style>
--- components/shared/Header.php
+++ components/shared/Header.php
@@ -1,37 +1,15 @@
 <!-- Header     -->
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-            <div class="navbarLeft col-md-3">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <a class="navbar-brand" href="#">LOGO</a>
-                <button class="btn btn-primary" id="menu-toggle">Toggle Menu</button>
-            </div>
-            
-
-            <form class="form-inline col-md-5">
-                    <input class="form-control" id="Search" type="search" placeholder="Busar...">
-            </form>
-
-            <div class="collapse navbar-collapse col-md-4" id="navbarTogglerDemo03">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Entra o registrate</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Mi carrito</a>
-                    </li>
-                </ul>
-
-            </div>
-        </nav>
+    <div class="row col-12" style="color:white;">
+        <div class="col-6">
+            <i class="fas fa-phone-alt"></i>0341-4214343 / 0341-4218027
+            <i class="fab fa-whatsapp"></i>3413213053
+            <i class="fas fa-envelope"></i>ventas@elintervalo.com
+        </div>
+        <div class="col-3">Chau</div>
+    </div>
+</nav>
 
 <style scope>
-#Search{
-    width: 100%;
-}
 
-.collapse ul{
-    margin-left: 40%;
-}
 </style>
