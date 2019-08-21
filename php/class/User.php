03ab1caf9d3d4bd4cd8ae6e479f9dadd07e89f9f
Facundo <facubargut@gmail.com>
2019-08-21 17:59:50 -0300

    Implementacion de clases

--- php/class/User.php
+++ php/class/User.php
@@ -1,8 +1,22 @@
 <?php
+    include "./conexion/conexion.php";
     class User {
-        private Name;
-        private Surname;
-        private DNI;
-        private Telephone_Number;
-        
+        private $Name;
+        private $Surname;
+        private $DNI;
+        private $Telephone_Number;
+
+        public function __contruct($name,$surname,$dni,$tel_num){
+            $this->Name = $name;    
+            $this->Surname = $surname;
+            $this->DNI = $dni;
+            $this->Telephone_Number = $tel_num;
+        }
+
+        public function Add(){
+            $db = new Conexion();
+
+        }
+
+
     }
