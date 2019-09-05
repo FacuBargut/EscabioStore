b681a6f3dbae814ddf9a6420bcc32be631cea272
Facundo <facubargut@gmail.com>
2019-09-05 14:12:01 -0300

    Creado

---
+++ php/script/LoginUser.php
@@ -0,0 +1,23 @@
+<?php
+    include "../class/User.php";
+    //Obtengo datos desde ajax
+    $UserEmail = $_POST['Email'];
+    $UserPass = $_POST['Pass'];
+
+    $UserLogued = User::SearchUserByEmailAndPass($UserEmail,$UserPass);
+    if($UserLogued){
+        //Inicio sesion
+        session_start();
+        $objUser = new User($UserLogued->Nombre,$UserLogued->Apellido,$UserLogued->Mail,$UserLogued->Password,$UserLogued->Activado);
+        $_SESSION['usuario'] = $objUser;
+        
+        echo "Sesion iniciada";
+
+    }else{
+        echo "Usuario no existe";
+    }
+
+    exit;
+
+
+?>
