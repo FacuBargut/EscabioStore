8aef2225efef22e79994804b0f3de990d89b8345
Facu <facubargut@gmail.com>
2019-09-09 02:10:11 -0300

    commi

--- components/admin/navbar.php
+++ components/admin/navbar.php
@@ -1,7 +1,23 @@
-<nav class="navbar navbar-dark bg-primary">
-  <a class="navbar-brand">Navbar</a>
-  <form class="form-inline">
-    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-  </form>
+<nav class="navbar navbar-expand-lg navbar-light bg-primary border-bottom">
+	<ul class="navbar-nav mr-auto">
+		<li class="nav-item">
+    		<a class="nav-link" href="#">Panel Administrador</a>
+    	</li>
+	</ul>
+
+	<div class="collapse navbar-collapse" id="navbarSupportedContent">
+	  <ul class="navbar-nav ml-auto mt-2 mt-lg-0">
+	    <li class="nav-item dropdown">
+	      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	        <?php echo $_SESSION['usuario']->getName()." ".$_SESSION['usuario']->getSurname();?>
+	      </a>
+	      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+	        <a class="dropdown-item" href="#">Cerrar</a>
+	        <a class="dropdown-item" href="#">Another action</a>
+	        <div class="dropdown-divider"></div>
+	        <a class="dropdown-item" href="#">Something else here</a>
+	      </div>
+	    </li>
+	  </ul>
+	</div>
 </nav>
--- components/admin/sidebar.php
+++ components/admin/sidebar.php
@@ -1,6 +1,6 @@
-<div class="col-md-3">
-    <ul>
-        <li>Clientes</li>
-        <li>Productos</li>
-    </ul>
+<div id="sidebar" class="bg-light col-md-2" style="min-height: 100vh;">
+	<ul class="list-group list-group-flush">
+		<li style="border-right:0;border-left:0;border-radius: 0;" class="list-group-item list-group-item-action bg-light">Clientes</li>
+		<li style="border-right:0;border-left:0;border-radius: 0;" class="list-group-item list-group-item-action bg-light">Productos</li>
+	</ul>
 </div>
